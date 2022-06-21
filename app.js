59fd17fa2224624eb003cf7416c690eb1bd511ed
samc <92285153+Samclaxton@users.noreply.github.com>
2022-06-21 13:25:25 +1200

    Completed task ii. to obtain expense figures

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const fs = require('fs');
 
 let data = fs.readFileSync('./data.json');
 let accounts = JSON.parse(data);
-// console.log(accounts);
 
 // Variable initialisation
 let totalrevenue = 0;
 
-// Calculate total revenue figure
+// i. Calculate total revenue figure:
 // Extract "revenue" entries, then sum to get total
 accounts.data
-    .filter(hi => hi.account_category == 'revenue')
-    .forEach(hi=> {
-        totalrevenue += hi.total_value;
+    .filter(entry => entry.account_category == 'revenue')
+    .forEach(entry=> {
+        totalrevenue += entry.total_value;
     });
 
-// Next step...
-
 console.log(totalrevenue)
+
+// Variable initialisation
+let expenses = 0;
+
+// ii. Calculate total expenses:
+// Extract "expense" entries, then sum to get total
+accounts.data
+    .filter(entry => entry.account_category == 'expense')
+    .forEach(entry=> {
+        expenses += entry.total_value;
+    });
+
+console.log(expenses)
