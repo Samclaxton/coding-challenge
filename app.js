1b30f6f4a18c7819eeac7d7c83fd7a0676c57ebd
samc <92285153+Samclaxton@users.noreply.github.com>
2022-06-29 14:56:52 +1200

    Added task iii. Gross Profit Margin to app.js file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let data = fs.readFileSync('./data.json');
-let accounts = JSON.parse(data);
+let data = fs.readFileSync('./data.json'); // Accesses the data.json file
+let accounts = JSON.parse(data); 
 
 // Variable initialisation
 let totalrevenue = 0;
@@ -14,17 +14,53 @@ accounts.data
         totalrevenue += entry.total_value;
     });
 
-console.log(totalrevenue)
-
-// Variable initialisation
-let expenses = 0;
+console.log(`Revenue: $${totalrevenue}`)
 
 // ii. Calculate total expenses:
 // Extract "expense" entries, then sum to get total
+
+let expenses = 0;
+
 accounts.data
     .filter(entry => entry.account_category == 'expense')
     .forEach(entry=> {
         expenses += entry.total_value;
     });
 
-console.log(expenses)
+console.log(`Expenses: $${expenses.toFixed()}`)
+
+/* iii. Calculate Gross Profit Margin:
+Gross Profit Margin = (Revenue - Cost of Goods Sold) / Revenue * 100
+*/
+
+// Variable initialisation
+let totalValueSales = 0;
+
+/* Step One:
+ Extract "Total Sales" 
+*/
+accounts.data
+    .filter(entry => entry.account_type == 'sales')
+    .forEach(entry=> {
+        totalValueSales += entry.total_value; 
+    });
+
+console.log(`Total Value Sales: $${totalValueSales}`)
+
+/* Step Two:
+Extract Cost of Goods Sold - All 'debit' transactions
+*/
+let COGS = 0;
+
+accounts.data
+    .filter(entry => entry.value_type == 'debit')
+    .forEach(entry=> {
+        COGS += entry.total_value; 
+    });
+
+  grossProfitMargin = (totalValueSales - COGS)/totalrevenue * 100
+  console.log(`Gross Profit Margin: ${grossProfitMargin.toFixed()}%`) 
+
+
+
+
